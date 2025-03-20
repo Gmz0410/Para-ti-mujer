0a2555d4a0833e6e2fad0aff5cc97d19eb86225c
Gmz0410 <Kenideagn@gmail.com>
2025-03-19 20:09:32 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const mensaje = "🌸 En el vasto universo, donde el tiempo no se detiene, tú eres la estrella que brilla con luz propia, sin que nada te frene. ✨" +
                     " He querido crear algo especial para ti, un pequeño gesto que ilumine tu día y te haga sonreír.💖" +
-                    " Aunque las palabras no siempre alcanzan para expresar, quiero que sepas cuánto significas para mí. No eres solo un pensamiento pasajero, sino un capítulo esencial en mi historia sin fin. 💞";
+                    " Aunque las palabras no siempre alcanzan para expresar, quiero que sepas cuánto significas para mí. No eres solo un pensamiento pasajero, sino un capítulo esencial en una historia que no tiene final. 💞";
     const elemento = document.querySelector('.mensaje p');
     let index = 0;
 
